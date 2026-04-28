cfaae2f590d38260c515558398a70e51bae61960
Cristóbal Suárez <cristobal2590@gmail.com>
2026-04-28 22:50:12 +0200

    comentarios_script_01

--- FOG_Server/scripts/fog-wakeup.sh
+++ FOG_Server/scripts/fog-wakeup.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-# --- CONFIGURACIÓN ---
+# --- Configuración de credenciales---
 DB_USER="root"
 DB_PASS="" # Pon la contraseña si tiene
 ROUTER_USER="usuario-wol"
 ROUTER_IP="10.2.7.3"
 
-# --- FUNCIONES DE CONSULTA SQL ---
+# https://www.reddit.com/r/bash/comments/hui3hd/i_dont_understand_0_1_2_variable_in_bash_shell/?tl=es-es
+
+
+# --- Funciones SQL para conseguir las MAC ---
+# $1 aquí representa el primer argumento enviado a la FUNCIÓN (Nombre del Host).
 get_macs_by_host() {
     mysql -u$DB_USER -p$DB_PASS fog -N -B -e "SELECT m.hmMAC FROM hosts h JOIN hostMAC m ON h.hostID = m.hmHostID AND m.hmPrimary = '1' WHERE h.hostName = '$1';"
 }
 
+# $1 aquí representa el nombre del grupo pasado a la función.
 get_macs_by_group() {
     mysql -u$DB_USER -p$DB_PASS fog -N -B -e "SELECT m.hmMAC FROM groups g JOIN groupMembers gm ON g.groupID = gm.gmGroupID JOIN hostMAC m ON gm.gmHostID = m.hmHostID AND m.hmPrimary = '1' WHERE g.groupName = '$1';"
 }
 
-# --- LÓGICA DE EJECUCIÓN ---
+# --- Ejecución: Case para hosts "h" o grupos "h". Si no pones nada válido, te muestra cómo usar el script y se detiene.
+# --- Si no encuentra MACs se indica.
+# $0: Nombre del script | $1: Primer argumento (-h o -g) | $2: Segundo argumento (Valor)
 case $1 in
     -h) MACS=$(get_macs_by_host "$2") ;;
     -g) MACS=$(get_macs_by_group "$2") ;;
@@ -29,14 +36,18 @@ fi
 
 # --- ENVÍO AL MIKROTIK ---
 for mac in $MACS; do
-    # Limpiar formato de MAC (Asegurar mayúsculas y dos puntos)
+    # NORMALIZACIÓN DE MAC:
+    # 1. Cambia guiones por dos puntos (tr '-' ':')
+    # 2. Convierte minúsculas a mayúsculas (tr '[:lower:]' '[:upper:]')
+    # Esto es necesario porque MikroTik no acepta formatos como 00-aa-11.
+    # https://stackoverflow.com/questions/23178769/unix-tr-command-to-convert-lower-case-to-upper-and-upper-to-lower-case
     clean_mac=$(echo $mac | tr '-' ':' | tr '[:lower:]' '[:upper:]')
     echo "Enviando comando WOL para: $clean_mac"
 
-    # Comando dinámico que se ejecuta íntegramente en el MikroTik
-    # Nota: Escapamos el $ de $i para que Bash no lo procese
+    # SCRIPT INTERNO MIKROTIK (RouterOS):
+    # Recorre todas las interfaces activas (running=yes) y envía el paquete WOL.
     MIKROTIK_CMD=":foreach i in=[/interface find where running=yes] do={ /tool wol mac=$clean_mac interface=[/interface get \$i name] }"
 
-    # Ejecución vía SSH
+    # Ejecución vía SSH. En nuestro caso tenemos configurado la Clave Pública con el router
     ssh $ROUTER_USER@$ROUTER_IP "$MIKROTIK_CMD"
 done
